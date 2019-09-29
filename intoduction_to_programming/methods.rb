40c6237cff482ef1cca4f630e08c31bcd10ad30b
vipin saini <vipin@jaaga.in>
2019-09-29 19:50:36 +0530

    learned methods

---
+++ intoduction_to_programming/methods.rb
@@ -0,0 +1,22 @@
+# methods
+
+# defining a method
+def sayMyName
+    puts "vipin saini"
+end
+
+# method with parameter
+def sayAnotherName(name)
+    puts name
+end
+
+def cal_sum(num1,num2)
+    return num1 + num2
+end
+
+# calling a method
+sayMyName
+sayAnotherName(234)
+
+result = cal_sum(5,7)
+puts result
