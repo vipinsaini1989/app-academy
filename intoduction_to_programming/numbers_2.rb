74f1939894fb740574c8cd233967808159033bad
vipin saini <vipin@jaaga.in>
2019-09-29 11:16:55 +0530

    learned some more arithmetic operators

--- intoduction_to_programming/numbers_2.rb
+++ intoduction_to_programming/numbers_2.rb
@@ -6,3 +6,13 @@ puts 24 == 24
 # != not equal to comparison
 puts 43 != 23
 puts 44 != 44
+
+# > greater than
+puts 4 > 5
+
+# < smaller than
+puts 6 < 4
+
+# >= greater than equal to
+puts 7 >= 8
+puts 7 >= 6
