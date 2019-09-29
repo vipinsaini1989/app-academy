85c1c80f3a32bbc37cf30fb11949a3483e6a60e5
vipin saini <vipin@jaaga.in>
2019-09-29 12:32:40 +0530

    learned about strings

---
+++ intoduction_to_programming/strings_1.rb
@@ -0,0 +1,18 @@
+# get length of string
+puts "vipin saini".length
+
+# get index of string
+puts "vipin"[0]
+puts "vipin"[-1] # get last char
+
+
+# print command other than puts
+# prints strings without formatting
+print "hello"
+print "world \n"
+
+# p command
+# print type of data as well
+p "hello"
+p "78"
+p 78
---
+++ intoduction_to_programming/strings_2.rb
@@ -0,0 +1,21 @@
+# printing chars using while loop
+str = "vipin"
+
+index = 0
+while index < str.length
+    puts str[index]
+    index += 1
+end
+
+
+
+# create function for printing chars
+def printStr(str1)
+    i = 0
+    while i < str1.length
+        puts str1[i]
+        i += 1
+    end
+end
+
+printStr('saini')
