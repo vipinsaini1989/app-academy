202326dfb5a8e9f3b6765bfdc0f501d48a28563c
vipin saini <vipin@jaaga.in>
2019-09-29 11:53:44 +0530

    learned logical operators

---
+++ intoduction_to_programming/number_3.rb
@@ -0,0 +1,18 @@
+# ! NOT
+# reverse the given condition
+puts !(3 > 4)
+
+# && AND
+# Always checks for both true
+puts (4 * 4) > 18 && 5 > 3
+puts true && true
+puts true && false
+puts false && true
+puts false && false
+
+# || OR
+# Always checks for one true
+puts true || true
+puts true || false
+puts false || true
+puts false || false
