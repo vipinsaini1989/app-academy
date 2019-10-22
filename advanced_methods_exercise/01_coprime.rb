d7f1901dafba95b90184515526b59897f79950ad
vipin saini <vipin@jaaga.in>
2019-10-22 20:20:22 +0530

    solved advanced methods problems

---
+++ advanced_methods_exercise/01_coprime.rb
@@ -0,0 +1,39 @@
+# Write a method, coprime?(num_1, num_2), that accepts two numbers as args.
+# The method should return true if the only common divisor between the two numbers is 1.
+# The method should return false otherwise. For example coprime?(25, 12) is true because
+# 1 is the only number that divides both 25 and 12.
+
+
+def coprime?(num1,num2)
+    
+    num1_factors = find_all_factors(num1)
+    num2_factors = find_all_factors(num2)
+
+    checker = Hash.new(0)
+
+    num1_factors.each { |factor| checker[factor] += 1 }
+    num2_factors.each { |factor| checker[factor] += 1 }
+
+    checker.each_value do |val|
+        return false if val > 1
+    end
+
+    return true
+end
+
+def find_all_factors(num)
+    factors = []
+    (2...num).each do |el|
+        if num % el == 0
+            factors << el
+        end
+    end
+
+    return factors
+end
+
+
+p coprime?(25, 12)    # => true
+p coprime?(7, 11)     # => true
+p coprime?(30, 9)     # => false
+p coprime?(6, 24)     # => false
